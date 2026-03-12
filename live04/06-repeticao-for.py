7e85d03f1d939badae9bb217476f74ce7b4791d8
Fernando Leonid <fernandoleonid@gmail.com>
2026-03-12 23:37:43 +0000

    Utilizando a estrutura de repetição FOR

---
+++ live04/06-repeticao-for.py
@@ -0,0 +1,10 @@
+alunos = ['Ana', 'Marta', 'Mario', 'Leonid','José', 'Hugo', 'Andy']
+
+# contador = 0
+# tamanho = len(alunos)
+# while contador < tamanho:
+#     print (f'nome do aluno: {alunos[contador]}')
+#     contador += 1
+
+for aluno in alunos:
+    print (f'nome do aluno: {aluno}')
