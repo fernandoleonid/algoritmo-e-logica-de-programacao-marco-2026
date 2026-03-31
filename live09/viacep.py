5327fd9366b4f33b222f30ea3e4aba90e1a2f758
Fernando Leonid <fernandoleonid@gmail.com>
2026-03-31 22:52:02 +0000

    viaCEP

---
+++ live09/viacep.py
@@ -0,0 +1,20 @@
+import requests
+
+def consultar_cep (cep):
+    end_point = f'https://viacep.com.br/ws/{cep}/json/'
+    response = requests.get(end_point)
+
+    if response.status_code == 200:
+        data = response.json()
+        if 'erro' not in data:
+            return data
+        else:
+            return {'erro': 'CEP não encontrado'}
+    else:
+        return {'erro': 'Erro de solicitação'}
+
+
+cep = input ('Digite um cep para pesquisar: ')
+info = consultar_cep(cep)
+
+print (info)
